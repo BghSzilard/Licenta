af397a49a390810c061a662fef604667989c960c
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-12-12 15:00:41 +0200

    Added possibility to check if the verified cpp compiles

--- Solution/src/main.cpp
+++ Solution/src/main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
-    std::cout << "Hello world!";
+    const char* inputFile = "FileToCorrect.cpp";
+
+    std::string command = "clang++ " + std::string(inputFile); +" -o test";
+
+    int result = std::system(command.c_str());
+
+    if (result == 0) {
+        std::cout << "Compilation successful.\n";
+    }
+    else {
+        std::cout << "Compilation failed.\n";
+    }
+
     return 0;
 }
