f506265d29bdd159b54f5bc9d24ecead60cc56f1
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2024-01-23 16:19:38 +0200

    implemented functions to perform full cleaning of every project, such that only cpp, header and text files remain

--- Solution/Engine/AutoCorrector/AutoCorrector/FileProcessor.cs
+++ Solution/Engine/AutoCorrector/AutoCorrector/FileProcessor.cs
@@ -1,4 +1,5 @@
-﻿using SharpCompress.Archives;
+﻿using SharpCompress;
+using SharpCompress.Archives;
 using System.IO.Compression;
 
 namespace AutoCorrector;
@@ -7,13 +8,21 @@ public class FileProcessor
 {
     public void ExtractZip(string sourcePath, string destinationPath)
     {
-        ZipFile.ExtractToDirectory(sourcePath, destinationPath);
+        try
+        {
+            ZipFile.ExtractToDirectory(sourcePath, destinationPath, true);
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.ToString());
+        }
     }
 
     public void ExtractRar(string sourcePath, string destinationPath)
     {
-        using (var archive = ArchiveFactory.Open(sourcePath))
+        try
         {
+            using var archive = ArchiveFactory.Open(sourcePath);
             foreach (var entry in archive.Entries)
             {
                 if (!entry.IsDirectory)
@@ -26,7 +35,128 @@ public class FileProcessor
                 }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
     }
+
+    public void ExtractArchivesRecursively(string rootDirectory)
+    {
+        string[] zipFiles = Directory.GetFiles(rootDirectory, "*.zip");
+        string[] rarFiles = Directory.GetFiles(rootDirectory, "*.rar");
+
+        foreach (string zipFile in zipFiles)
+        {
+            ExtractZip(zipFile, rootDirectory);
+            File.Delete(zipFile);
+        }
+
+        foreach (string rarFile in rarFiles)
+        {
+            ExtractRar(rarFile, rootDirectory);
+            File.Delete(rarFile);
+        }
+
+        string[] subDirectories = Directory.GetDirectories(rootDirectory);
+
+        foreach (string subDirectory in subDirectories)
+        {        
+            ExtractArchivesRecursively(subDirectory);
+        }
+
+        Array.Clear(zipFiles);
+        Array.Clear(rarFiles);
+
+        zipFiles = Directory.GetFiles(rootDirectory, "*.zip");
+        rarFiles = Directory.GetFiles(rootDirectory, "*.rar");
+
+        if (zipFiles.Length > 0 || rarFiles.Length > 0)
+        {
+            ExtractArchivesRecursively(rootDirectory);
+        }
+    }
+
+    public void DeleteSubdirectories(string rootDirectory)
+    {
+        var subdirectories = Directory.GetDirectories(rootDirectory);
+        foreach (var subDirectory in subdirectories)
+        {
+            DeleteDirectory(subDirectory);
+        }
+    }
+
+    public void Asd(string rootDirectory)
+    {
+        var subdirectories = Directory.GetDirectories(rootDirectory);
+        List<string> extensions = new List<string>() { ".txt", ".h", ".hpp", ".cpp" };
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(rootDirectory);
+
+        var files = directoryInfo.GetFiles();
+
+        foreach (var file in files)
+        {
+            string extension = file.Extension;
+
+            if (!extensions.Contains(extension))
+            {
+                file.Delete();
+            }
+        }
+
+        foreach (var subdirectory in subdirectories)
+        {
+            ExtractFiles(rootDirectory, subdirectory, extensions);
+        }
+    }
+
+    public void ExtractFiles(string rootDirectory, string currentDirectory, List<string> extensions)
+    {
+        var subdirectories = Directory.GetDirectories(currentDirectory);
+
+        List<string> files = new List<string>();
+
+        foreach (var extension in extensions)
+        {
+            foreach (var file in (Directory.GetFiles(currentDirectory, $"*{extension}")))
+            {
+                files.Add(file);
+            }
+        }
+
+        foreach (var file in files)
+        {
+            File.Copy(file, Path.Combine(rootDirectory, Path.GetFileName(file)), true);
+        }
+
+        foreach (var subdirectory in subdirectories)
+        {
+            ExtractFiles(rootDirectory, subdirectory, extensions);
+        }
+
+        DeleteDirectory(currentDirectory);
+        
+    }
+    public void DeleteDirectory(string rootDirectory)
+    {
+        string[] files = Directory.GetFiles(rootDirectory);
+        string[] dirs = Directory.GetDirectories(rootDirectory);
+
+        foreach (string file in files)
+        {
+            File.SetAttributes(file, FileAttributes.Normal);
+            File.Delete(file);
+        }
+
+        foreach (string dir in dirs)
+        {
+            DeleteDirectory(dir);
+        }
+
+        Directory.Delete(rootDirectory, false);
+    }
+
     public List<string> GetSubdirectoryNames(string path)
     {
         var directory = new DirectoryInfo(path);
--- Solution/Engine/AutoCorrector/AutoCorrector/Program.cs
+++ Solution/Engine/AutoCorrector/AutoCorrector/Program.cs
@@ -5,8 +5,19 @@ class Program
 
     public static async Task Main()
     {
-        StudentManager studentManager = new StudentManager();
-        await studentManager.GetNames();
+        FileProcessor processor = new FileProcessor();
+        var root = "C:\\Users\\sziba\\Desktop\\MyFolder";
 
+        processor.ExtractRar("C:\\Users\\sziba\\Desktop\\AF_2023-Testare Parțial 1 - 17 noiembrie 1600-53027.zip",
+            "C:\\Users\\sziba\\Desktop\\MyFolder");
+
+        var subd = Directory.GetDirectories(root);
+
+
+        foreach (var sub in subd)
+        {
+            processor.ExtractArchivesRecursively(sub);
+            processor.Asd(sub);
+        }
     }
 }
