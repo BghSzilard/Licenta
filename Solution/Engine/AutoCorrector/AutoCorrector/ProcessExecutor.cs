d77ae887dec8df26d9e9433b2599cd30e33fc66e
Balogh Szilard <z004w26z@ROBAVGF7GWFG01>
2024-03-18 13:13:20 +0200

    Implemented ProcessExecutor

---
+++ Solution/Engine/AutoCorrector/AutoCorrector/ProcessExecutor.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace AutoCorrectorEngine;
+
+public class ProcessExecutor
+{
+    public string ExecuteProcess(string command, string arguments, string input)
+    {
+        ProcessStartInfo processStartInfo = new ProcessStartInfo
+        {
+            FileName = command,
+            Arguments = arguments,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using (Process process = new Process())
+        {
+            process.StartInfo = processStartInfo;
+            process.Start();
+
+            process.StandardInput.WriteLine(input);
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+
+            string stdout = process.StandardOutput.ReadToEnd();
+            string stderr = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+
+            return stdout;
+        }
+    }
+}
