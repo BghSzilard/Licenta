08d4aae734d75ff7c6d13d050dfb381cb9b132e7
Balogh Szilard <z004w26z@ROBAVGF7GWFG01>
2024-03-21 11:48:30 +0200

    Added python script that can translate input text from any language to English

---
+++ Solution/Engine/AutoCorrector/AutoCorrector/Translate.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace AutoCorrectorEngine;
+
+public class Translate
+{
+    public string TranslateToEnglish(string text)
+    {
+        ProcessExecutor processExecutor = new ProcessExecutor();
+        return processExecutor.ExecuteProcess("python", "C:\\Users\\z004w26z\\Desktop\\Translate.py", text);
+    }
+}
